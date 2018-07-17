f9d97cabbf3b23db0d7f8639d2425e01cb5eb983
Eufrat Tsaqib <eutsat@gmail.com>
2018-07-17 13:01:57 +0700

    Add PDF processing code

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const extract = require("extract-zip");
 const fileUpload = require("express-fileupload");
 const rimraf = require("rimraf");
 const cors = require("cors");
+// const jsPDF = require("node-jspdf");
 
 const app = express();
 const port = 8000;
@@ -15,15 +16,55 @@ app.listen(port, () => {
 
 app.use(fileUpload());
 
+const getBase64 = file => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        console.log(reader.result);
+    };
+    reader.onerror = error => {
+        console.log("Error: ", error);
+    };
+};
+
+const filePath = path.join(__dirname, "..", "public", "plate.zip");
+const target = path.join(__dirname, "..", "public", "plate");
+
 const production = false;
 if (!production) app.use(cors());
+
+const getPlateFiles = () => {
+    let files = [];
+
+    fs.readdirSync(target).forEach(file => {
+        files.push(file);
+    });
+
+    let output = [];
+
+    files = files.map(file => {
+        const filename = file.split(".")[0];
+
+        const imageExtension = ["jpg", "jpeg", "png", "gif"];
+        const fileExtension = file.split(".")[1];
+
+        if (imageExtension.includes(fileExtension)) {
+            output.push({
+                image: `/plate/${file}`,
+                content: filename,
+                validation: "false",
+                blur: "false"
+            });
+        }
+    });
+
+    return output;
+};
 
 app.post("/upload", (req, res) => {
     if (!req.files) return res.status(400).send("No files were uploaded.");
 
     let plateFile = req.files.plateFile;
-    const filePath = path.join(__dirname, "..", "public", "plate.zip");
-    const target = path.join(__dirname, "..", "public", "plate");
 
     rimraf(target, () => {
         console.log(`Folder ${target} removed`);
@@ -35,31 +76,7 @@ app.post("/upload", (req, res) => {
                 if (err) {
                     res.status(500).send("Error on extracting file.");
                 } else {
-                    let files = [];
-
-                    fs.readdirSync(target).forEach(file => {
-                        files.push(file);
-                    });
-
-                    let output = [];
-
-                    files = files.map(file => {
-                        const filename = file.split(".")[0];
-
-                        const imageExtension = ["jpg", "jpeg", "png", "gif"];
-                        const fileExtension = file.split(".")[1];
-
-                        if (imageExtension.includes(fileExtension)) {
-                            output.push({
-                                image: `/plate/${file}`,
-                                content: filename,
-                                validation: "false",
-                                blur: "false"
-                            });
-                        }
-                    });
-
-                    // console.log(output);
+                    const output = getPlateFiles();
 
                     res.status(200).send(JSON.stringify({ data: output }));
                 }
@@ -67,3 +84,40 @@ app.post("/upload", (req, res) => {
         });
     });
 });
+
+/*
+app.get("/pdf", (req, res) => {
+    const pdf = new jsPDF("landscape");
+    let y = 0;
+
+    const output = getPlateFiles();
+
+    for (let item of output) {
+        let x = 0;
+
+        for (let key in item) {
+            item.hasOwnProperty(key);
+
+            x += 20;
+            if (key === "image") {
+                const imgData =
+                    "data:image/jpeg;base64," +
+                    getBase64(`/plate/${item[key]}.jpg`);
+                pdf.addImage(imgData, "JPEG", x, y);
+                x += 30;
+            } else {
+                pdf.text(key, x, y);
+                x += 30;
+                pdf.text(item[key], x, y);
+                x += 30;
+            }
+        }
+        pdf.line(20, 20, 60, 20);
+        y += 20;
+    }
+
+    // pdf.addImage(imgData, "JPEG", 0, 0, 0, 0);
+    pdf.save("download.pdf");
+});
+
+*/
